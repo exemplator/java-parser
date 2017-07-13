414bcf465a77a97aa79a3154311d8d82687d3324
LeanderK <Leander.Kurscheidt@gmx.de>
2017-07-13 19:06:54 +0200

    added position

---
+++ Language/Java/Position.hs
@@ -0,0 +1,24 @@
+module Position where
+
+import           Text.Parsec.Pos hiding (Column, Line)
+
+type Line = Int
+type Column = Int
+
+-- | Position in a Text Line Column
+data Position = Position Line Column
+    deriving (Show, Read)
+
+type Start = Position
+type End = Position
+
+-- | A segment has a Start and an End
+data Segment = Segment Start End
+
+class HasSegment a where
+    getSegment :: a -> Segment
+
+sourcePosToSegment :: SourcePos -> SourcePos -> Segment
+sourcePosToSegment start end = Segment (toPostion start) (toPostion end)
+    where
+        toPostion sourcePos = Position (sourceLine sourcePos) (sourceColumn sourcePos)
