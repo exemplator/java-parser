d9fb228b53591b09ee0c4fa309c24d4cc03e2432
LeanderK <Leander.Kurscheidt@gmx.de>
2017-08-08 16:50:49 +0200

    changed lib to helper

--- Language/Java/Syntax.hs
+++ Language/Java/Syntax.hs
@@ -46,13 +46,14 @@ module Language.Java.Syntax
     , MethodInvocation(..)
     , module Language.Java.Syntax.Exp
     , module Language.Java.Syntax.Types
-    , module Language.Java.Lib
+    , module Language.Java.Helper
     ) where
 
 import           Data.Data
 import           Data.Maybe
 import           GHC.Generics               (Generic)
 
+import           Language.Java.Helper
 import           Language.Java.Syntax.Exp
 import           Language.Java.Syntax.Types
 
